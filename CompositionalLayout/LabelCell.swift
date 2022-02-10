86c8e38b2ef9de0749b41e85067d5a30d3779e5f
Rahul <thengadir@gmail.com>
2022-02-10 11:14:48 +0530

    create LabelCell (assign custom class add reuse identifier to CollectionViewCell)

---
+++ CompositionalLayout/LabelCell.swift
@@ -0,0 +1,51 @@
+//
+//  LabelCell.swift
+//  CompositionalLayout
+//
+//  Created by Rahul Thengadi on 10/02/22.
+//
+
+import UIKit
+
+class LabelCell: UICollectionViewCell {
+    
+    public lazy var textLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        return label
+    }()
+    
+    // coming from programatic UI setup
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+    }
+    
+    // coming from Storyboard
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    // helper initializer method
+    private func commonInit() {
+        textLabelConstraints()
+    }
+    
+    private func textLabelConstraints() {
+        // 1
+        addSubview(textLabel)
+        
+        // 2
+        // we will handle layout using Auto Layout not autoresizing mask
+        textLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        // 3
+        // setup required constraints
+        NSLayoutConstraint.activate([
+            textLabel.centerYAnchor.constraint(equalTo: centerYAnchor),
+            textLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+            textLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 8),
+            textLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -8)
+        ])
+    }
+}
